5004417b26bf61ae7da4dd490f6c015379bedc00
James Glover <3057283g@student.gla.ac.uk>
2025-02-15 17:33:02 +0000

    Managed to understand populate rango and updated views and likes

--- populate_rango.py
+++ populate_rango.py
@@ -40,6 +40,21 @@ def populate():
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
+    python_cat = Category.objects.get_or_create(name='Python')[0]
+    python_cat.views = 128
+    python_cat.likes = 64
+    python_cat.save()
+
+    django_cat = Category.objects.get_or_create(name='Django')[0]
+    django_cat.views = 64
+    django_cat.likes = 32
+    django_cat.save()
+
+    other_frameworks_cat = Category.objects.get_or_create(name='Other Frameworks')[0]
+    other_frameworks_cat.views = 32
+    other_frameworks_cat.likes = 16
+    other_frameworks_cat.save()
+
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
